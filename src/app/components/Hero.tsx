79e8557069e4323253cc340969d37d2b5339df68
Apisit Angsuphan <60085220+sussus003@users.noreply.github.com>
2025-02-13 20:52:42 +0700

    complete Hero section(Home)

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import {HiArrowNarrowRight} from 'react-icons/hi'
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
 
 function Hero() {
-  return (
-    <div className='w-[100%] h-screen banner'>Her</div>
-  )
+  return <div id='hero' className="w-[100%] h-screen banner">
+    <div className="max-w-[1000px] mx-auto h-full px-8 flex flex-col justify-center text-gray-100">
+      <p className="text-[#ececec]">Hi, My name is</p>
+      <h1 className="text-4xl sm:text-6xl font-bold">
+        Apisit  Angsuphan
+      </h1>
+      <h2 className="text-4xl">
+        I'm a Front End Developer.
+      </h2>
+      <p className="text-[#ececec] pt-4 max-w-[700px]">
+      Having worked in tutoring, online sales, compliance inspector, organizational coordination, and web development, 
+      </p>
+      <p className="text-[#ececec] py-3 max-w-[700px]">
+      I discovered that Front-End Development is my true passion.
+      </p>
+      <div>
+        <button className="bg-teal-600 text-white rounded-full border-2 px-6 py-3 my-2 flex justify-between items-center hover:bg-teal-500 hover:border-teal-600">View Work <span className="duration-300"><HiArrowNarrowRight className="ml-3"/></span> </button>
+      </div>
+    </div>
+  </div>;
 }
 
-export default Hero
+export default Hero;
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,16 +1,22 @@
-'use client'
-import React,{useState} from "react";
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { FaBars, FaTimes,FaFacebook, FaInstagram,FaGithub } from "react-icons/fa";
+import {
+  FaBars,
+  FaTimes,
+  FaFacebook,
+  FaInstagram,
+  FaGithub,
+} from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import logo from "./../assets/logo.png";
 
 function Navbar() {
-    const [navbar, setNavbar] = useState<boolean>(false);
-    const handleClick = ()=> {
-        setNavbar(!navbar);
-    }
+  const [navbar, setNavbar] = useState<boolean>(false);
+  const handleClick = () => {
+    setNavbar(!navbar);
+  };
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#08111e] text-gray-300">
       <div>
@@ -23,37 +29,83 @@ function Navbar() {
       {/* Menu pc */}
       <div>
         <ul className="md:flex hidden ">
-            <li><Link href={"/"}>Home</Link></li>
-            <li><Link href={"/"}>About</Link></li>
-            <li><Link href={"/"}>Skills</Link></li>
-            <li><Link href={"/"}>Work</Link></li>
-            <li><Link href={"/"}>Contact</Link></li>
-          
+          <li>
+            <Link href={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link href={"/"}>About</Link>
+          </li>
+          <li>
+            <Link href={"/"}>Skills</Link>
+          </li>
+          <li>
+            <Link href={"/"}>Work</Link>
+          </li>
+          <li>
+            <Link href={"/"}>Contact</Link>
+          </li>
         </ul>
       </div>
 
       {/* Hamburger */}
-        <div className="md:hidden z-10" onClick={handleClick}>
-           {!navbar ? <FaBars/>:<FaTimes/> }
-        </div>
+      <div className="md:hidden z-10" onClick={handleClick}>
+        {!navbar ? <FaBars /> : <FaTimes />}
+      </div>
       {/* Menu mobile */}
-      <ul className={!navbar ?"hidden" : "absolute top-0 left-0 w-full h-screen bg-[#08111e] flex flex-col justify-center items-center"}>
-            <li className="py-6 text-1xl uppercase font-semibold"><Link href={"/"}>Home</Link></li>
-            <li className="py-6 text-1xl uppercase font-semibold"><Link href={"/"}>About</Link></li>
-            <li className="py-6 text-1xl uppercase font-semibold"><Link href={"/"}>Skills</Link></li>
-            <li className="py-6 text-1xl uppercase font-semibold"><Link href={"/"}>Work</Link></li>
-            <li className="py-6 text-1xl uppercase font-semibold"><Link href={"/"}>Contact</Link></li>
-          
-        </ul>
+      <ul
+        className={
+          !navbar
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#08111e] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-1xl uppercase font-semibold">
+          <Link href={"/"}>Home</Link>
+        </li>
+        <li className="py-6 text-1xl uppercase font-semibold">
+          <Link href={"/"}>About</Link>
+        </li>
+        <li className="py-6 text-1xl uppercase font-semibold">
+          <Link href={"/"}>Skills</Link>
+        </li>
+        <li className="py-6 text-1xl uppercase font-semibold">
+          <Link href={"/"}>Work</Link>
+        </li>
+        <li className="py-6 text-1xl uppercase font-semibold">
+          <Link href={"/"}>Contact</Link>
+        </li>
+      </ul>
       {/* Social Icons ( Left side) */}
-      <div className="flex fixed flex-col top-[35%] left-0 ">
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0 ">
         <ul>
-            <li className="flex "><Link href={'/'}>Facebook<FaFacebook/></Link></li>
-            <li><Link href={'/'}>Facebook<FaFacebook/></Link></li>
-            <li><Link href={'/'}>Facebook<FaFacebook/></Link></li>
-            <li><Link href={'/'}>Facebook<FaFacebook/></Link></li>
-            
-            {/* <li><Link href={'/'}>< /Link></li> */}
+          <li className="w-[160px] h-[60px] flex justify-between ml-[-100px] hover:ml-[-10] duration-300 bg-[#0866FF] rounded-full rounded-l-none">
+            <Link
+              className="flex justify-between items-center w-full text-gray-300"
+              href={"/"}
+            >
+              Facebook
+              <FaFacebook size={30} />
+            </Link>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between ml-[-100px] hover:ml-[-10] duration-300 bg-slate-900 rounded-full rounded-l-none mt-5">
+            <Link
+              className="flex justify-between items-center w-full text-gray-300"
+              href={"/"}
+            >
+              Github
+              <FaGithub size={30} />
+            </Link>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between ml-[-100px] hover:ml-[-10] duration-300 bg-pink-600 rounded-full rounded-l-none mt-5">
+            <Link
+              className="flex justify-between items-center w-full text-gray-300"
+              href={"/"}
+            >
+              Email
+              <HiOutlineMail size={30} />
+            </Link>
+          </li>
+          
         </ul>
       </div>
     </div>
