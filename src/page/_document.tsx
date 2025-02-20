69a90fe802c75994d36351aae30c0ac304658bd2
Apisit Angsuphan <60085220+sussus003@users.noreply.github.com>
2025-02-21 02:48:17 +0700

    add custom Document for Google Analytics integration

---
+++ src/page/_document.tsx
@@ -0,0 +1,30 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* ใส่ Google Analytics ที่นี่ */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-8K048NLKTC"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-8K048NLKTC');
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
